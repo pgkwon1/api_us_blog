c6b02773ac9ee6630578a822858902adee759056
pgkwon1@naver.com <pgkwon1@naver.com>
2023-03-20 22:31:44 +0900

    Feat: add posts table add column

--- models/Posts.model.ts
+++ models/Posts.model.ts
@@ -2,17 +2,26 @@ import { Association, DataTypes, Model } from "sequelize";
 import { sequelize } from ".";
 import Likes from "./Likes.model";
 import Tags from "./Tags.model";
-import Users from "./Users.model";
 
-interface PostsAttribute {
+const category = ["기술", "직장", "잡담"] as const;
+type Category = (typeof category)[keyof typeof category];
+
+interface IPostsDto {
   id: string;
   author: string;
+  title: string;
+  contents: string;
   like: number;
+  category: Category;
 }
-class Posts extends Model<PostsAttribute> {
+class Posts extends Model<IPostsDto> {
   public id: string;
 
   public author: string;
+
+  public title: string;
+
+  public contents: string;
 
   public like: number;
 
@@ -38,8 +47,21 @@ Posts.init(
       type: DataTypes.UUID,
       allowNull: false,
     },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
     like: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.ENUM,
+      values: ["기술", "직장", "잡담"],
       allowNull: false,
     },
   },
