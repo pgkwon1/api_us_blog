4e500b42c07170e10f28e8c77976676dc8e80248
pgkwon1@naver.com <pgkwon1@naver.com>
2023-03-26 01:25:25 +0900

    Feat: get csrf token

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import "express-async-errors";
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 
 import indexRouter from "./routes/index";
 import memberRouter from "./routes/member";
 import postsRouter from "./routes/posts";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   bodyParser.urlencoded({
@@ -17,7 +19,15 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(morgan("combined"));
 }
+app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(csrf({ cookie: true }));
+
+app.use("/getCsrf", (req: Request, res: Response) => {
+  res.send({
+    CSRF_TOKEN: req.csrfToken(),
+  });
+});
 app.use("/", indexRouter);
 app.use("/member", memberRouter);
 app.use("/post", postsRouter);
