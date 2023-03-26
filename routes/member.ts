29877e442fc1a1e7141cd7f17a0e3b86130ba353
pgkwon1@naver.com <pgkwon1@naver.com>
2023-03-26 16:06:43 +0900

    Feat: csrfProtection

--- routes/member.ts
+++ routes/member.ts
@@ -1,25 +1,33 @@
 import express, { Request, Response } from "express";
 import UserController from "../controllers/Users.controller";
 import UserMiddleWare from "../middleware/Users.middleware";
+import csrf from "csurf";
+
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  try {
-    // 입력값 검사
-    await UserMiddleWare.validateRegisterBody(req.body);
-    const User = new UserController();
+const csrfProtection = csrf({ cookie: true });
 
-    await User.register(req.body);
-  } catch (err) {
-    if (err instanceof ReferenceError) {
-      err.message = "회원가입에 실패하였습니다 관리자에게 문의해주세요";
+router.post(
+  "/register",
+  csrfProtection,
+  async (req: Request, res: Response) => {
+    try {
+      // 입력값 검사
+      await UserMiddleWare.validateRegisterBody(req.body);
+      const User = new UserController();
+
+      await User.register(req.body);
+    } catch (err) {
+      if (err instanceof ReferenceError) {
+        err.message = "회원가입에 실패하였습니다 관리자에게 문의해주세요";
+      }
+      res.send(
+        JSON.stringify({
+          message: err.message,
+        })
+      );
     }
-    res.send(
-      JSON.stringify({
-        message: err.message,
-      })
-    );
   }
-});
+);
 
 export default router;
