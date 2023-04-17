f10cb1bd16ddee506f9515933bdd7545c3a9e845
pgkwon1@naver.com <pgkwon1@naver.com>
2023-04-17 22:55:51 +0900

    Fix: domain 업데이트

--- domain/controllers/Posts.ts
+++ domain/controllers/Posts.ts
@@ -1,5 +1,8 @@
+import IWriteDto from "../../dto/post/WriteDto";
+
 export interface IPostsControllerDomain {
-  getPostsList(): Promise<object>;
   getPost(): Promise<object>;
+  getPostListByTag(tagName: string, page: number): Promise<object>;
   getUserPostList(): Promise<object>;
+  store(data: IWriteDto): Promise<string>;
 }
--- domain/services/Posts.ts
+++ domain/services/Posts.ts
@@ -1,4 +1,6 @@
 export interface IPostsServiceDomain {
+  getPost(): Promise<object>;
   getPostList(): Promise<object>;
   getUserPostList(): Promise<object>;
+  store(data: IWriteDto): Promise<object>;
 }
