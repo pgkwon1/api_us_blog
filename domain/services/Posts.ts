907a2a91577632df981b33a9ad4e9ca5d16d999d
pgkwon1@naver.com <pgkwon1@naver.com>
2023-04-18 00:29:43 +0900

    Fix: 인터페이스에 메소드 추가.

--- domain/services/Posts.ts
+++ domain/services/Posts.ts
@@ -4,5 +4,6 @@ export interface IPostsServiceDomain {
   getPost(): Promise<object>;
   getPostList(): Promise<object>;
   getUserPostList(): Promise<object>;
+  getPostListByCategory(category: string, page: number): Promise<object>;
   store(data: IWriteDto): Promise<object>;
 }
