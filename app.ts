fd8ff74161a74ebc04d013f186863ae660c27569
pgkwon1@naver.com <pgkwon1@naver.com>
2023-05-19 19:02:25 +0900

    Feat: like router 추가

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import { logger } from "./config/winston";
 import indexRouter from "./routes/index";
 import memberRouter from "./routes/member";
 import postsRouter from "./routes/posts";
+import likesRouter from "./routes/likes";
 import csrf from "csurf";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
@@ -82,6 +83,8 @@ app.use("/getCsrf", (req: Request, res: Response) => {
 app.use("/", indexRouter);
 app.use("/member", memberRouter);
 app.use("/post", postsRouter);
+app.use("/like", likesRouter);
+
 app.use(async (err, req, res, next) => {
   if (err.message === 403) res.send("비정상적인 접근입니다.");
   if (err instanceof ReferenceError === true) {
